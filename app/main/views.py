a86de250b2b3609744ea8f0d7a08acf0411d3bd5
BlandineK <blandineuwase222@gmail.com>
2019-09-14 21:16:03 +0200

    add body tag

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_movies
+from .request import get_newss
 # Views
 @app.route('/')
 def index():
@@ -8,8 +8,9 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    title = 'Home - you are most  welcome to the best News Website Online'
-    return render_template('index.html',title=title)
+    anitha_newss=get_newss()
+    title = 'You are most  welcome to the best News Website Online'
+    return render_template('index.html',title=title,news=anitha_newss)
 @app.route('/news/<int:news_id>')
 def news(news_id):
 
--- app/request.py
+++ app/request.py
@@ -26,3 +26,28 @@ def get_newss(category):
 
 
     return news_results
+def process_results(news_list):
+    '''
+    Function  that processes the news result and transform them to a list of Objects
+
+    Args:
+        news_list: A list of dictionaries that contain news details
+
+    Returns :
+        news_results: A list of news objects
+    '''
+    news_results = []
+    for news_item in news_list:
+      id = source_item.get('id')
+      name = source_item.get('name')
+      description = source_item.get('description')
+      url = source_item.get('url')
+      category = source_item.get('category')
+      language = source_item.get('language')
+      country = source_item.get('country')
+      if poster:
+        news_object=News(id,name,description,url,category,language,country)
+        news_results.append(news_object)
+    return news_results  
+
+
