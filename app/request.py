4fade67c2b225eb33874ea3500dc9594c22d15e3
BlandineK <blandineuwase222@gmail.com>
2019-09-14 20:06:07 +0200

    edit the file of request

--- app/request.py
+++ app/request.py
@@ -8,20 +8,20 @@ api_key = app.config['NEWS_API_KEY']
 
 # Getting the movie base url
 base_url = app.config["NEWS_API_BASE_URL"]
-def get_news(category):
+def get_newss(category):
     '''
     Function that gets the json response to our url request
     '''
-    get_news_url = base_url.format(category,api_key)
+    get_newss_url = base_url.format(category,api_key)
 
-    with urllib.request.urlopen(get_news_url) as url:
-        get_news_data = url.read()
-        get_news_response = json.loads(get_news_data)
+    with urllib.request.urlopen(get_newss_url) as url:
+        get_newss_data = url.read()
+        get_newss_response = json.loads(get_newss_data)
 
         news_results = None
 
-        if get_news_response['results']:
-            news_results_list = get_news_response['results']
+        if get_newss_response['results']:
+            news_results_list = get_newss_response['results']
             news_results = process_results(news_results_list)
 
 
