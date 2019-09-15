97f5a4e0e05b6456c6bf7c1930d5bcf318040b4e
BlandineK <blandineuwase222@gmail.com>
2019-09-15 19:50:22 +0200

    import config options

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask   
 from config import config_options 
 from flask_bootstrap import Bootstrap
+
 # Initializing application
 app = Flask(__name__, instance_relative_config = True)
 
@@ -11,3 +12,16 @@ app.config.from_pyfile('config.py')
 # Initializing Flask Extensions
 bootstrap = Bootstrap(app)
 # from app import views
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+
+    return app
