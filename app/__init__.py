c9cdff3101f4ddf87f5162b98350eb906dafe785
BlandineK <blandineuwase222@gmail.com>
2019-09-15 20:06:40 +0200

    making identantion

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,8 @@
 from flask import Flask   
 from config import config_options 
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap  
 
-# Initializing application
-app = Flask(__name__, instance_relative_config = True)
-
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
-
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+bootstrap = Bootstrap()
 # from app import views
 def create_app(config_name):
 
